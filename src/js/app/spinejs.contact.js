68891617d754344dae5da516f1357c65c5eb66d9
Juan Pablo Feliciani <jpfeliciani+git@gmail.com>
2013-01-04 17:12:37 -0300

    Fixed path and errors (?) on the js files

--- src/js/app/spinejs.contact.js
+++ src/js/app/spinejs.contact.js
@@ -1,3 +1,4 @@
+/*global $:false, jQuery:true, Spine:true */
 var Contact = Spine.Model.sub();
 Contact.configure("Contact", "contact_name", "contact_phone", "contact_message");
 
@@ -8,19 +9,19 @@ Contact.configure("Contact", "contact_name", "contact_phone", "contact_message")
 Contact.include({
 	validate: function(){
 		//console.log(!_.isNumber(this.contact_phone));
-		if ( !this.contact_name )
-			 return "Name is required";
-		if ( !this.contact_phone )
-			return "Only numeric value | Required";
-		if ( !this.contact_message )
-			return "It's required you to provide a message";
+		if ( !this.contact_name ){return "Name is required";}
+			 
+		if ( !this.contact_phone ){return "Only numeric value | Required";}
+			
+		if ( !this.contact_message ) {return "It's required you to provide a message";}
+			
 	}
 });
 
 var ContactControllerApp = Spine.Controller.sub({
 	init: function(){
 		Contact.bind("error", function(rec, msg) {
-			return alert("Fail to submit - " + msg);
+			return window.alert("Fail to submit - " + msg);
 		});
 	},
 
@@ -34,7 +35,7 @@ var ContactControllerApp = Spine.Controller.sub({
 		var contact = Contact.fromForm(e.target);
 		//contact.save();
 		if (contact.save()) {
-			alert(	"Name: " + contact.contact_name +
+			window.alert(	"Name: " + contact.contact_name +
 					";\nNumber: " + contact.contact_phone +
 					";\nMessage: " + contact.contact_message 
 					);
